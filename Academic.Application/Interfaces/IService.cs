ab96979efe067278fb8ccff42ae3fc4b277a6d72
KhaledMidian <khaledwaheed002349@gmail.com>
2024-06-28 23:22:15 +0300

    Creating IService interface

---
+++ Academic.Application/Interfaces/IService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academic.Application.Interfaces
+{
+    internal interface IService<T,T2> where T : class where T2 : class
+    {
+        public List<T> GetAll();
+        public List<T> GetById(string Id, string? include);
+        public void Delete(T2 dTO);
+        public void Add(T2 dTO);
+        public void Update(T2 dTO);
+    }
+}
